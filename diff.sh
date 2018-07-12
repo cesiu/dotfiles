6d85c6090c304efd162ad4f4d1a20637bb672ad9
cesiu <christopher@get2thecrossing.com>
2018-07-12 10:03:50 -0700

    Fixed bad arguments to colordiff workaround.

--- diff.sh
+++ diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/diff --unified=3 $1 $2 | sed 's/^-/\x1b[31m-/;s/^+/\x1b[32m+/;s/^@/\x1b[36m@/;s/$/\x1b[0m/'
+/bin/diff --unified=3 $@ | sed 's/^-/\x1b[31m-/;s/^+/\x1b[32m+/;s/^@/\x1b[36m@/;s/$/\x1b[0m/'
